ac6e4945609c2f342838107a7f4e960b0547dcc5
Thoranin Chaiyakam <ThoraninC@gmail.com>
2021-07-01 18:23:32 +0700

    Commiting Gambler And Condition Abstract

---
+++ Assets/Script/ChipZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChipZone : MonoBehaviour
+{
+    [SerializeField]
+    private Image chipPrefab = default;
+    [SerializeField]
+    private PayOutCondition[] payOutConditions = default;
+    [SerializeField]
+    private Gambler[] stakeHolders = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/PayOutCondition/PayOutCondition.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PayOutCondition
+{
+    public abstract float payRatio { get; protected set; }
+
+    public abstract bool checkPayOutCondition();
+}
